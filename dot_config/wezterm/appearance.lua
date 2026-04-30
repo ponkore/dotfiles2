c5c880e96272767e3dff56ba04bb5c6944e038fa
Masao KATO <ponkore@gmail.com>
2026-04-30 20:47:55 +0900

    fix(wezterm): macOS 判定を string.find() 形式に修正
    wezterm.target_triple の型の違いに影響されないよう、
    メソッド形式 :find() から string.find() 関数形式に変更し、
    検索文字列も "apple-darwin" から "darwin" に緩和。

--- dot_config/wezterm/appearance.lua
+++ dot_config/wezterm/appearance.lua
@@ -10,7 +10,7 @@ function M.apply(config)
     { family = "HackGen Console NF" },
     { family = "SauceCodePro Nerd Font Mono" },
   })
-  local is_mac = wezterm.target_triple:find("apple-darwin") ~= nil
+  local is_mac = string.find(wezterm.target_triple, "darwin") ~= nil
   config.font_size = is_mac and 14.0 or 12.0
   config.adjust_window_size_when_changing_font_size = false
   config.treat_east_asian_ambiguous_width_as_wide = false
